1276abd7a4e78a9f47654d8d1801c9b332e499f1
Victor Carnavali <victorcarnavali.dev@gmail.com>
2024-12-25 17:06:22 -0300

    create new exercises using javaFX

--- HelloWorldJavaFX/src/helloworldjavafx/FXMLDocumentController.java
+++ HelloWorldJavaFX/src/helloworldjavafx/FXMLDocumentController.java
@@ -9,6 +9,7 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 /**
@@ -18,12 +19,12 @@ import javafx.scene.control.Label;
 public class FXMLDocumentController implements Initializable {
     
     @FXML
-    private Label label;
+    private Label lblMessage;
+    private Button btnButton;
     
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
+    private void clickButton(ActionEvent event) {
+        lblMessage.setText("Hello World");
     }
     
     @Override
