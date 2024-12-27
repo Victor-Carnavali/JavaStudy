4965287dd53b4c8f916a61fdb897f2433452e3fa
Victor Carnavali <victorcarnavali.dev@gmail.com>
2024-12-27 12:32:16 -0300

    create a simple exercise of primitive types

---
+++ PrimitiveTypes/src/primitivetypes/PrimitiveTypes.java
@@ -0,0 +1,27 @@
+
+package primitivetypes;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Victor
+ */
+public class PrimitiveTypes {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("O nome do aluno: ");
+        String nome = scanner.nextLine();
+        System.out.print("A nota do aluno: ");
+        float nota = scanner.nextFloat();
+        scanner.
+
+        System.out.format("A nota de %s e %.2f \n",nome,nota);
+    }
+    
+}
