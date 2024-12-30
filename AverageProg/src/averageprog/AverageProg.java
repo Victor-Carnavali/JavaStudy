225e85752c4cd27fe4bab3317fd4174ad7bc84c8
Victor Carnavali <victorcarnavali.dev@gmail.com>
2024-12-30 12:28:34 -0300

    simples and compound conditions

---
+++ AverageProg/src/averageprog/AverageProg.java
@@ -0,0 +1,46 @@
+
+package averageprog;
+import java.time.Year;
+import java.util.Scanner;
+public class AverageProg {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner scanner = new Scanner(System.in);
+        
+        // Simple condition
+        
+        System.out.print("Put first value: ");
+        float firstValue = scanner.nextFloat();
+        System.out.print("Put second value: ");
+        float secondValue = scanner.nextFloat();
+        
+        float averageResult = (firstValue + secondValue) / 2;
+        System.out.printf("Your average is: %.2f \n",averageResult);
+        
+        if (averageResult >= 6) {
+            System.out.println("Great job");
+        }
+        
+    
+        // Compound condition 
+        /*int systemYear = Year.now().getValue();
+        
+        System.out.println("What year are you born? ");
+        int yearBorn = scanner.nextInt();
+        int age = systemYear - yearBorn;
+        
+        if (age >= 18){
+            System.out.println("You have "+ age +" years old and you are in the legal age :D");
+        }
+        else {
+            System.out.println("You have "+ age +" years old and you are not the age legal :C");
+        }*/
+        
+        
+        
+    }
+}
