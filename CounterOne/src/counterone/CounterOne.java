a077a6c915ac4e0b90189874405fe49559c3b15d
Victor Carnavali <victorcarnavali.dev@gmail.com>
2024-12-31 14:22:09 -0300

    create counter using while, continue and break

---
+++ CounterOne/src/counterone/CounterOne.java
@@ -0,0 +1,19 @@
+package counterone;
+
+public class CounterOne {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int cc = 0;
+
+        while (cc < 10) {
+            cc++;
+            if(cc == 5 || cc == 7){
+                break;
+            }
+            System.out.println("Somersault = " + cc);
+        }
+        
+    }
+
+}
