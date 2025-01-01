5d865b7ac5b0b74af99f9c21e6755fe8972b063f
Victor Carnavali <victorcarnavali.dev@gmail.com>
2025-01-01 15:24:53 -0300

    for repetition exercise

---
+++ RepetitionFor/src/repetitionfor/RepetitionFor.java
@@ -0,0 +1,15 @@
+
+package repetitionfor;
+
+public class RepetitionFor {
+
+    public static void main(String[] args) {
+        
+        for (int i = 1; i <= 3; i++){
+            for (int j = 0; j <= 2; j+=2){
+                System.out.println("Value of i: "+i+", value of j: "+j);
+            }
+        }
+    }
+    
+}
