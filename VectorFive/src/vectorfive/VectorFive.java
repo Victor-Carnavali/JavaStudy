a495dab9e67b5632e10b401c925f012acc4fdbd3
Victor Carnavali <victorcarnavali.dev@gmail.com>
2025-01-02 13:17:13 -0300

    finish the exercise

--- VectorFive/src/vectorfive/VectorFive.java
+++ VectorFive/src/vectorfive/VectorFive.java
@@ -1,20 +1,15 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package vectorfive;
 
-/**
- *
- * @author Victor
- */
+import java.util.Arrays;
+
 public class VectorFive {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        int v[] = new int[20];
+        Arrays.fill(v, 0);
+        for (int value : v) {
+            System.out.println(value);
+        }
     }
-    
+
 }
