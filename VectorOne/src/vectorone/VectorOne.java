9cfb04e8b14b8d04355d583e5240de47f5d25949
Victor Carnavali <victorcarnavali.dev@gmail.com>
2025-01-02 11:27:02 -0300

    first vector exercise

---
+++ VectorOne/src/vectorone/VectorOne.java
@@ -0,0 +1,14 @@
+package vectorone;
+
+public class VectorOne {
+
+    public static void main(String[] args) {
+        int n[] = {3, 2, 8, 7, 5, 4};
+        
+        System.out.println("Length of vector is: "+n.length+" positions");
+        for (int c = 0; c <= n.length-1; c++){
+            System.out.println(n[c]);
+        }
+    }
+
+}
