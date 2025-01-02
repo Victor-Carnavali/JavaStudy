8449e00bedf5ae7d2602c4fd48f341b12fb1363d
Victor Carnavali <victorcarnavali.dev@gmail.com>
2025-01-02 15:34:10 -0300

    create file function test

---
+++ FunctionTestOne/src/functiontestone/FunctionTestOne.java
@@ -0,0 +1,17 @@
+
+package functiontestone;
+
+public class FunctionTestOne {
+    
+    static int sum(int x, int y){
+        int s = x + y;
+        return s;
+    }
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int sm = sum(5,2);
+        System.out.println(sm);
+    }
+    
+}
