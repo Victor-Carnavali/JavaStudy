bf177cf283de1ab904cb1b9fec2ca8fe94903313
Victor Carnavali <victorcarnavali.dev@gmail.com>
2025-02-03 13:00:36 -0300

    variables

--- PrimeiroProjeto/src/primeiroprojeto/PrimeiroProjeto.java
+++ PrimeiroProjeto/src/primeiroprojeto/PrimeiroProjeto.java
@@ -1,7 +1,15 @@
 package primeiroprojeto;
 public class PrimeiroProjeto {
     public static void main(String[] args) {
-        System.out.println("hello world!");
+        System.out.println("Este e o screen match");
+        
+        String nomeDoFilme = "Top Gun Maverick";
+        System.out.println("Nome do filme: " + nomeDoFilme);
+        int anoDeLancamento = 2022;
+        System.out.println("Ano de lancamento: " + anoDeLancamento);
+        boolean incluidoNoPlano = true;
+        double notaFilme = 7.9;
+        System.out.println("Nota do filme: " + notaFilme);
+        
     }
-    
 }
