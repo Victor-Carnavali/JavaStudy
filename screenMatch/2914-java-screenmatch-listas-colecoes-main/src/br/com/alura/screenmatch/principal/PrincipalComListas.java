75717b43fcf82d7f9b4d90125d72e79508b726a7
Victor Carnavali <victorcarnavali.dev@gmail.com>
2025-02-04 13:07:48 -0300

    another commit :D

--- screenMatch/2914-java-screenmatch-listas-colecoes-main/src/br/com/alura/screenmatch/principal/PrincipalComListas.java
+++ screenMatch/2914-java-screenmatch-listas-colecoes-main/src/br/com/alura/screenmatch/principal/PrincipalComListas.java
@@ -4,10 +4,7 @@ import br.com.alura.screenmatch.modelos.Filme;
 import br.com.alura.screenmatch.modelos.Serie;
 import br.com.alura.screenmatch.modelos.Titulo;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
 
 public class PrincipalComListas {
     public static void main(String[] args) {
@@ -32,7 +29,7 @@ public class PrincipalComListas {
             }
         }
 
-        List<String> buscaPorArtista = new ArrayList<>();
+        List<String> buscaPorArtista = new LinkedList<>();
         buscaPorArtista.add("Adam Sandler");
         buscaPorArtista.add("Kevin James");
         buscaPorArtista.add("David Spade");
