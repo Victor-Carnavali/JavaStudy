80ddf0ef226ae82be17a38fc726f2857ae553e30
Victor Carnavali <victorcarnavali.dev@gmail.com>
2025-02-06 18:27:50 -0300

    add new catch exception IllegalArgumentException

--- screenMatch/src/victor/javastudy/screenmatch/principal/PrincipalComBusca.java
+++ screenMatch/src/victor/javastudy/screenmatch/principal/PrincipalComBusca.java
@@ -20,33 +20,35 @@ public class PrincipalComBusca {
         String busca = leitura.nextLine();
 
         String endereco = "http://www.omdbapi.com/?t=" + busca + "&apikey=e119a4bd";
-
-        HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create(endereco))
-                .build();
-
-        HttpResponse<String> response = client
-                .send(request, HttpResponse.BodyHandlers.ofString());
-
-        String json = response.body();
-        System.out.println(json);
-
-        Gson gson = new GsonBuilder()
-                .setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE)
-                .create();
-
-        TituloOmdb meuTituloOmdb = gson.fromJson(json, TituloOmdb.class);
-        System.out.println(meuTituloOmdb);
-
         try {
+            HttpClient client = HttpClient.newHttpClient();
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create(endereco))
+                    .build();
+
+            HttpResponse<String> response = client
+                    .send(request, HttpResponse.BodyHandlers.ofString());
+
+            String json = response.body();
+            System.out.println(json);
+
+            Gson gson = new GsonBuilder()
+                    .setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE)
+                    .create();
+
+            TituloOmdb meuTituloOmdb = gson.fromJson(json, TituloOmdb.class);
+            System.out.println(meuTituloOmdb);
+
             Titulo meuTitulo = new Titulo(meuTituloOmdb);
             System.out.println("Titulo ja convertido");
             System.out.println("Titulo ja convertido: " + meuTitulo);
         } catch (NumberFormatException e) {
             System.out.println("Aconteceu um erro: " + e.getMessage());
+        } catch (IllegalArgumentException e) {
+            System.out.println("Para palavras com espaco utilize '-' exemplo: top-gun | about-time");
+        } catch (Exception e) {
+            System.out.println("Aconteceu algo, porem nao sei o que aconteceu!");
         }
-
         System.out.println("O programa finalizou corretamente");
 
     }
