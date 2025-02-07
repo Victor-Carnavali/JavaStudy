4e4b6d83782174a2f2cbb0f90560fa6a89b16690
Victor Carnavali <victorcarnavali.dev@gmail.com>
2025-02-07 13:45:08 -0300

    buscador de cep

--- buscador/src/ConsultaCep.java
+++ buscador/src/ConsultaCep.java
@@ -1,3 +1,6 @@
+import com.google.gson.Gson;
+
+import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
@@ -8,17 +11,22 @@ import static java.net.http.HttpClient.newHttpClient;
 public class ConsultaCep {
 
     public Endereco buscaEndereco(String cep){
-        URI endereco = "https://viacep.com.br/ws/" + cep + "/json";
+        URI endereco = URI.create("https://viacep.com.br/ws/" + cep + "/json");
 
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(endereco)
                 .build();
 
-        HttpResponse<String> response = HttpClient;
-                .newHttpClient()
-                .send(request, HttpResponse.BodyHandlers.ofString());
-
+        HttpResponse<String> response = null;
+        try {
+            response = HttpClient
+                    .newHttpClient()
+                    .send(request, HttpResponse.BodyHandlers.ofString());
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException("Nao consegui obter o endereco a partir desse cep...");
+        }
         return new Gson().fromJson(response.body(), Endereco.class);
+
     }
 }
--- buscador/src/Principal.java
+++ buscador/src/Principal.java
@@ -1,5 +1,8 @@
+
 public class Principal {
     public static void main(String[] args) {
-        
+        ConsultaCep consulta = new ConsultaCep();
+        Endereco novoEndereco = consulta.buscaEndereco("01001000");
+        System.out.println(novoEndereco);
     }
 }
