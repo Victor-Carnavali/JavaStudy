44a57ee58c9489434a7ac80ad489e9715de90bb5
Victor Carnavali <victorcarnavali.dev@gmail.com>
2025-02-10 13:26:22 -0300

    add new project with spring java

---
+++ screenmovies/src/main/java/victor/javastudy/screenmovies/ScreenmoviesApplication.java
@@ -0,0 +1,13 @@
+package victor.javastudy.screenmovies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenmoviesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreenmoviesApplication.class, args);
+	}
+
+}
---
+++ screenmovies/src/test/java/victor/javastudy/screenmovies/ScreenmoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package victor.javastudy.screenmovies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScreenmoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
