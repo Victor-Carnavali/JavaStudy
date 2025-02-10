c542cdc2e765dc4b1aa465e409b6f49dadd8659e
Victor Carnavali <victorcarnavali.dev@gmail.com>
2025-02-10 17:10:03 -0300

    using jackson library maven

---
+++ screenmovies/src/main/java/victor/javastudy/screenmovies/model/DadosSerie.java
@@ -0,0 +1,6 @@
+package victor.javastudy.screenmovies.model;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+
+public record DadosSerie(@JsonAlias("Title")  String titulo,@JsonAlias("totalSeasons") Integer totalTemporadas,@JsonAlias("imdbRating") String avaliacao) {
+}
