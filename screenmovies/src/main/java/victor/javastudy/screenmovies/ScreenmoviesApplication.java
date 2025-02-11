4a0d6644f5030f91008cebe3f8d8e64097af8fa7
Victor Carnavali <victorcarnavali.dev@gmail.com>
2025-02-11 11:08:57 -0300

    add new feature DadosTemporada, shearch for all episodes in all seasons of the serie

--- screenmovies/src/main/java/victor/javastudy/screenmovies/ScreenmoviesApplication.java
+++ screenmovies/src/main/java/victor/javastudy/screenmovies/ScreenmoviesApplication.java
@@ -5,8 +5,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import victor.javastudy.screenmovies.model.DadosEpisodio;
 import victor.javastudy.screenmovies.model.DadosSerie;
+import victor.javastudy.screenmovies.model.DadosTemporada;
 import victor.javastudy.screenmovies.service.ConsumoApi;
 import victor.javastudy.screenmovies.service.ConverteDados;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 public class ScreenmoviesApplication implements CommandLineRunner {
@@ -26,5 +30,13 @@ public class ScreenmoviesApplication implements CommandLineRunner {
 		json = consumoApi.obterDados("https://www.omdbapi.com/?t=the+oc&y=2003&season=1&episode=3&apikey=e119a4bd");
 		DadosEpisodio dadosEpisodio = conversor.obterDados(json, DadosEpisodio.class);
 		System.out.println(dadosEpisodio);
+		List<DadosTemporada> temporadas = new ArrayList<>();
+
+		for (int i = 1; i <= dados.totalTemporadas(); i++){
+			json = consumoApi.obterDados("https://www.omdbapi.com/?t=the+oc&y=2003&season=" + i + "&apikey=e119a4bd");
+			DadosTemporada dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
+			temporadas.add(dadosTemporada);
+		}
+		temporadas.forEach(System.out::println);
 	}
 }
