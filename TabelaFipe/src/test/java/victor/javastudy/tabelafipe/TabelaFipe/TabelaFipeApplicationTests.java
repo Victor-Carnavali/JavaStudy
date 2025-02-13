cbd4745161eb3b79b9a9f9b89f03b78f387224bc
Victor Carnavali <victorcarnavali.dev@gmail.com>
2025-02-13 09:40:18 -0300

    add spring start generate

---
+++ TabelaFipe/src/test/java/victor/javastudy/tabelafipe/TabelaFipe/TabelaFipeApplicationTests.java
@@ -0,0 +1,13 @@
+package victor.javastudy.tabelafipe.TabelaFipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TabelaFipeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
